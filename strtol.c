55962e2290fbfbfe3f2cf80b94c048ab0b884baa
adamjedrzejec <adamjedrzejec@o2.pl>
2018-05-06 02:18:40 +0200

    still trying to make a base-detection function

--- strtol.c
+++ strtol.c
@@ -32,11 +32,59 @@ long strtol (const char *nPtr, char **endPtr, int base)
   }else
     sign = POSITIVE; /* if first non-blank character is not '+' nor '-', then we assume the number as a positive value, even though string may not be a number */
 
+
+  /* detection what is the base */
+  if (base == 0){
+    const char *positionBeforeBaseDetection;
+    positionBeforeBaseDetection = actualPosition;
+
+    if (!isdigit(*actualPosition))
+      return 0; /* string is not a number */
+    else if (*actualPosition == '0')
+    {
+      actualPosition++;
+      if (*actualPosition == 'o' || *actualPosition == 'O'){ /* we assume that octal system is written in '0o...' or '0O...' way */
+        actualPosition++;
+        if (*actualPosition >= '0' || *actualPosition <= '7')
+          base = 8;
+        else
+          return 0;
+      }
+      else if (*actualPosition == 'x' || *actualPosition == 'X'){ /* we assume that hexadecimal system is written in '0x...' or '0X...' way */
+        actualPosition++;
+        if (isdigit(*actualPosition) || ((*actualPosition >= 'A') && (*actualPosition <= 'F')))
+          base = 16;
+        else
+          return 0;
+      }
+      else
+        base = 10;
+    }else /* this condition contains: actualPosition is a digit and that digit is not 0 */
+      base = 10;
+
+    actualPosition = positionBeforeBaseDetection;
+
+  }
+
+
+
   if (base == 10){
     printf("base equals 10\n");
+  }else if (base == 8){
+    printf("base equals 8\n");
   }else if (base == 16){
     printf("base equals 16\n");
   }
+
+
+
+        /////////  *endPtr = (char *) actualPosition;
+
+
+
+
+
+
 
 
   return number;
